a877e04e5eb11dd6b6bb0a76374b20e6105f95b5
peter <Peter.Halada@bol-systemhaus.de>
2023-04-26 19:17:18 +0200

    Added create file

--- src/oop/MainOop.java
+++ src/oop/MainOop.java
@@ -1,6 +1,9 @@
 package oop;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 import oop.books.Author;
@@ -23,6 +26,20 @@ public class MainOop {
         kniznica.addBook(javaKniha);
         kniznica.addBook(detska);
         kniznica.addBook(detska2);
+
+
+        try {
+            File subor = new File("data" + File.separator + "kniznica.txt");
+
+            FileWriter fw = new FileWriter(subor);
+            fw.write("Antoine Exupery,Maly princ");
+            fw.close();
+
+        } catch (IOException e) {
+
+            System.out.println("Subor sa nepodarilo vytvorit " + e.getMessage());
+            return;
+        }
 
         try {
             List<Book> foundBooks = kniznica.findBookByAuthorSurname("Exup");
