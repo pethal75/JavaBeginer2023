7ee3b80a7595df40519d4bc7c2f3f4c39ea85513
peter <Peter.Halada@bol-systemhaus.de>
2023-05-03 18:38:58 +0200

    PhoneBook

--- src/phonebook/MainPhoneBook.java
+++ src/phonebook/MainPhoneBook.java
@@ -1,13 +1,31 @@
 package phonebook;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class MainPhoneBook {
     public static void main(String[] args) throws IOException {
         PhoneBook phoneBook = new PhoneBook();
         phoneBook.loadFromFile("data\\kontakty.txt");
-        phoneBook.printAllContacts();
 
-        // TODO Menu 1 - print contacts, 2 add contact, 3 search contact
+        // TODO Menu 1 - print contacts, 2 add contact, 3 search contact, 4 exit
+        Scanner scanner = new Scanner(System.in);
+
+        boolean exit = false;
+
+        while (exit == false) {
+            System.out.println("1.Vypisat");
+            System.out.println("2.pridat kontakt");
+            System.out.println("3.hladat kontakt");
+            System.out.println("4.koniec");
+
+            String volba = scanner.next();
+
+            if (volba.equals("1"))
+                phoneBook.printAllContacts();
+
+            if (volba.equals("4"))
+                exit = true;
+        }
     }
 }
--- src/phonebook/PhoneBook.java
+++ src/phonebook/PhoneBook.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,11 +33,31 @@ public class PhoneBook {
         fr.close();
     }
 
-    public void saveToFile(String fileName) {}
+    public void saveToFile(String fileName) throws IOException {
+        File file = new File(fileName);
+        FileWriter fw = new FileWriter(file);
 
-    public void addContact(Contact contact) {}
+        for(Contact contact : this.contacts)
+            fw.write( contact.getName() + ","
+                    + contact.getLastName() + ","
+                    + contact.getPhone() + ","
+                    + contact.getEmail() + "\n");
+
+        fw.close();
+    }
+
+    public void addContact(Contact contact) {
+        if (this.searchContact(contact.getName(), contact.getLastName()) == null)
+            this.contacts.add(contact);
+    }
 
     public Contact searchContact(String name, String lastName) {
+
+        for(Contact contact: this.contacts)
+            if (contact.getName().toLowerCase().equals(name.toLowerCase())
+                    && contact.getLastName().toLowerCase().equals(lastName.toLowerCase()))
+                return contact;
+
         return null;
     }
 
